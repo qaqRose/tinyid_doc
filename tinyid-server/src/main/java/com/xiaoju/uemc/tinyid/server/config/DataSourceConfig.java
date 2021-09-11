cc585ac78c7bfe4a385e6c2a3ad6b0355351cc93
qiu <singleqaq@gmail.com>
2021-09-12 02:11:22 +0800

    :book: 动态数据源注释

--- tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/config/DataSourceConfig.java
+++ tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/config/DataSourceConfig.java
@@ -18,6 +18,8 @@ import java.util.Map;
 
 /**
  * @author du_imba
+ *
+ * 数据源配置
  */
 @Configuration
 public class DataSourceConfig {
@@ -30,12 +32,21 @@ public class DataSourceConfig {
 
     private static final String DEFAULT_DATASOURCE_TYPE = "org.apache.tomcat.jdbc.pool.DataSource";
 
+    /**
+     * 数据源配置
+     * 动态数据源，如果有多个的话
+     * @return
+     */
     @Bean
     public DataSource getDynamicDataSource() {
         DynamicDataSource routingDataSource = new DynamicDataSource();
         List<String> dataSourceKeys = new ArrayList<>();
+
+        // 获取spring环境配置
         RelaxedPropertyResolver propertyResolver = new RelaxedPropertyResolver(environment, "datasource.tinyid.");
+        // names 为数据源名称， 多个用逗号隔开
         String names = propertyResolver.getProperty("names");
+        // 数据源类型
         String dataSourceType = propertyResolver.getProperty("type");
 
         Map<Object, Object> targetDataSources = new HashMap<>(4);
@@ -43,15 +54,26 @@ public class DataSourceConfig {
         routingDataSource.setDataSourceKeys(dataSourceKeys);
         // 多个数据源
         for (String name : names.split(SEP)) {
+            // 获取数据库连接配置
             Map<String, Object> dsMap = propertyResolver.getSubProperties(name + ".");
+            // 构造数据源
             DataSource dataSource = buildDataSource(dataSourceType, dsMap);
+            // 完善数据源配置
             buildDataSourceProperties(dataSource, dsMap);
+            // 添加到数据源map中
             targetDataSources.put(name, dataSource);
+            // key
             dataSourceKeys.add(name);
         }
         return routingDataSource;
     }
 
+    /**
+     * 完善数据源配置
+     * 设置更多的配置，当然前提是application.properties有配置
+     * @param dataSource
+     * @param dsMap
+     */
     private void buildDataSourceProperties(DataSource dataSource, Map<String, Object> dsMap) {
         try {
             // 此方法性能差，慎用
@@ -61,12 +83,20 @@ public class DataSourceConfig {
         }
     }
 
+    /**
+     * 构造数据源
+     * 只设置基本的参数：驱动、地址、账号密码
+     * @param dataSourceType
+     * @param dsMap
+     * @return
+     */
     private DataSource buildDataSource(String dataSourceType, Map<String, Object> dsMap) {
         try {
             String className = DEFAULT_DATASOURCE_TYPE;
             if (dataSourceType != null && !"".equals(dataSourceType.trim())) {
                 className = dataSourceType;
             }
+            // 获取application.properties的配置信息
             Class<? extends DataSource> type = (Class<? extends DataSource>) Class.forName(className);
             String driverClassName = dsMap.get("driver-class-name").toString();
             String url = dsMap.get("url").toString();
--- tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/config/DynamicDataSource.java
+++ tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/config/DynamicDataSource.java
@@ -1,5 +1,7 @@
 package com.xiaoju.uemc.tinyid.server.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 import java.util.List;
@@ -7,18 +9,29 @@ import java.util.Random;
 
 /**
  * @author du_imba
+ *
+ * 动态数据源
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
+    private static final Logger logger = LoggerFactory.getLogger(DynamicDataSource.class);
+
     private List<String> dataSourceKeys;
 
+    /**
+     * 获取动态数据源的key
+     * @return
+     */
     @Override
     protected Object determineCurrentLookupKey() {
+
         if(dataSourceKeys.size() == 1) {
             return dataSourceKeys.get(0);
         }
         Random r = new Random();
-        return dataSourceKeys.get(r.nextInt(dataSourceKeys.size()));
+        String key = dataSourceKeys.get(r.nextInt(dataSourceKeys.size()));
+        logger.info("dataSource Key:{}", key);
+        return key;
     }
 
     public List<String> getDataSourceKeys() {
